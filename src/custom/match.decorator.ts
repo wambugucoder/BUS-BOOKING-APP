2d01556c1f42084947d541bfd48d6c3fedb88c50
joswambugu <josphatwambugu77@gmail.com>
2020-09-29 14:55:50 +0300

    Third Commit

--- src/custom/match.decorator.ts
+++ src/custom/match.decorator.ts
@@ -1,24 +1,28 @@
-import {registerDecorator, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface} from 'class-validator';
+import {
+  registerDecorator, ValidationArguments, ValidationOptions,
+  ValidatorConstraint, ValidatorConstraintInterface,
+} from 'class-validator';
 
+// tslint:disable-next-line:function-name
 export function Match(property: string, validationOptions?: ValidationOptions) {
-    return (object: any, propertyName: string) => {
-        registerDecorator({
-            target: object.constructor,
-            propertyName,
-            options: validationOptions,
-            constraints: [property],
-            validator: MatchConstraint,
-        });
-    };
+  return (object: any, propertyName: string) => {
+    registerDecorator({
+      propertyName,
+      target: object.constructor,
+      options: validationOptions,
+      constraints: [property],
+      validator: MatchConstraint,
+    });
+  };
 }
 
-@ValidatorConstraint({name: 'Match'})
+@ValidatorConstraint({ name: 'Match' })
 export class MatchConstraint implements ValidatorConstraintInterface {
 
-    validate(value: any, args: ValidationArguments) {
-        const [relatedPropertyName] = args.constraints;
-        const relatedValue = (args.object as any)[relatedPropertyName];
-        return value === relatedValue;
-    }
+  validate(value: any, args: ValidationArguments) {
+    const [relatedPropertyName] = args.constraints;
+    const relatedValue = (args.object as any)[relatedPropertyName];
+    return value === relatedValue;
+  }
 
 }
