4d86d85706c90b4e866c15beb3aa14a6f5088c00
joswambugu <josphatwambugu77@gmail.com>
2020-10-28 12:04:48 +0300

    CI

--- src/cache/Cache.ts
+++ src/cache/Cache.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 const redisport: string = process.env.PORT_REDIS!;
 
 const opts: redis.ClientOpts = {
-  host:'192.168.99.100',
+  host:'redis',
   port: parseInt(redisport, 10),
 
 };
